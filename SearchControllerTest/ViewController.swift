9038aac5a4767655c22a4b7acea0ef8d7a1b5ef3
Boyd Timothy <btimothy@gmail.com>
2019-09-23 19:37:32 -0600

    Initial Commit

---
+++ SearchControllerTest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SearchControllerTest
+//
+//  Created by Boyd Timothy on 9/23/19.
+//  Copyright © 2019 Bluegrass Guru, LLC. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
