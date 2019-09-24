29b99742404a33f9babf397117557a64fc82c8a6
Boyd Timothy <btimothy@gmail.com>
2019-09-23 20:00:51 -0600

    Add UISearchController and UINavigationController

--- SearchControllerTest/ViewController.swift
+++ SearchControllerTest/ViewController.swift
@@ -10,12 +10,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    static var colors: [UIColor] = [.black, .blue, .brown, .cyan, .darkGray, .gray, .green, .magenta, .orange, .purple, .red]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let searchController = UISearchController()
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
     }
 
-
+    @IBAction func changeColorTapped(_ sender: Any) {
+        let randomIndex = Int.random(in: 0 ..< ViewController.colors.count)
+        view.backgroundColor = ViewController.colors[randomIndex]
+    }
+    
 }
 
